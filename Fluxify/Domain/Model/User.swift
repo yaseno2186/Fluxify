ee42ceb1b5bfb0eae1b6016a7b7b9b54dd40cfbc
Yass Almardoud <149265907+yaseno2186@users.noreply.github.com>
2026-01-18 09:58:29 +0100

    Update User.swift

--- Fluxify/Domain/Model/User.swift
+++ Fluxify/Domain/Model/User.swift
@@ -5,3 +5,15 @@
 //  Created by TA638 on 17.01.26.
 //
 
+import Foundation
+
+struct User {
+    let uid: UUID
+    //    var Lessons:
+    var isCompleted: Bool
+    var Email: String
+    var streak: Int?
+    var FirstName: String?
+    var LastName: String?
+    var phone: String?
+}
