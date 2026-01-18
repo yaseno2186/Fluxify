deadcaee2dded5db8638d2b5b46abd60a9304902
Kami <kamilchen8@gmail.com>
2026-01-18 10:32:14 +0100

    Good Luck, Guys ;)

--- Fluxify/Lesson/Hint.swift
+++ Fluxify/Lesson/Hint.swift
@@ -5,3 +5,21 @@
 //  Created by TA638 on 17.01.26.
 //
 
+import SwiftUI
+struct HintView: View {
+    var body: some View {
+        VStack{
+            Text("Everyday LifeHacks")
+                
+            
+        }
+    }
+    
+    
+
+    }
+
+
+#Preview {
+    HintView()
+}
